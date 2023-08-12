da4cf9f935400d6a2f604e065343c3c5cc70a660
Debora Peña <debo4978@gmail.com>
2023-08-11 23:28:29 -0300

    solucion

--- eventos.js
+++ eventos.js
@@ -1,8 +1,24 @@
-let cont = document.getElementById("cont")
+let cont = document.getElementById("cont");
+let btn = document.getElementById("btn");
 
-cont.addEventListener("click", let  = () => {alert('¡Hello! Soy el div')});
+cont.addEventListener("click", helloDiv)
+btn.addEventListener("click", remove)
+btn.addEventListener("click", let = () => { alert('Hola!') });
+
+
+function helloDiv() {
+ alert('¡Hello! Soy el div')
+}
+
+function remove() {
+    cont.removeEventListener("click", helloDiv)
+}
 
 
 
 
 
+
+
+
+    
